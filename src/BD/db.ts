5d8a08ab8151ed3f53ffefc856b747716b7ecbef
AMmetro <metro.trend.now@gmail.com>
2024-01-22 20:57:06 +0300

    setup ENV

--- src/BD/db.ts
+++ src/BD/db.ts
@@ -4,9 +4,7 @@ import { BlogDB } from '../models/blog/db/blog-db';
 import { PostDB } from '../models/post/db/post-db'; 
 
 dotenv.config()
-// const mongoURI = process.env.REMOUTE_MONGO_URL || "mongodb://0.0.0.0:27017"; 
-
-const mongoURI="mongodb+srv://metroexpress:suradet842@cluster0.gkpqpve.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = process.env.REMOUTE_MONGO_URL || "mongodb://0.0.0.0:27017"; 
 
 export const client = new MongoClient(mongoURI);
 
