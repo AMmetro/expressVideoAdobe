3048c099232961d4076f0c4bd3cf255e4377804c
AMmetro <metro.trend.now@gmail.com>
2024-02-18 17:56:43 +0300

    add

--- src/utils/sendResponse.ts
+++ src/utils/sendResponse.ts
@@ -2,22 +2,22 @@ import { ResultCode } from "../validators/error-validators";
 
 export function sendCustomError(res: any, result: any) {
   if (result.status === ResultCode.NotFound) {
-    res.status(404).send(`${result.errorMessage}`);
+    res.status(404).send(result.errorMessage);
     return;
   } else if (result.status === ResultCode.ClientError) {
-    res.status(400).send(`${result.errorMessage}`);
+    res.status(400).send(result.errorMessage);
     return;
   } else if (result.status === ResultCode.Unauthorised) {
-    res.status(401).send(`${result.errorMessage}`);
+    res.status(401).send(result.errorMessage);
     return;
   } else if (result.status === ResultCode.Forbidden) {
-    res.status(403).send(`${result.errorMessage}`);
+    res.status(403).send(result.errorMessage);
     return;
   } else if (result.status === ResultCode.Conflict) {
-    res.status(409).send(`${result.errorMessage}`);
+    res.status(409).send(result.errorMessage);
     return;
   } else if (result.status === ResultCode.ServerError) {
-    res.status(503).send(`${result.errorMessage}`);
+    res.status(503).send(result.errorMessage);
     return;
   }
 }
