44cc54cdd9c6a8f8374160ee4489aa235e0dfc5e
AMmetro <metro.trend.now@gmail.com>
2024-02-21 12:51:55 +0300

    delete comments

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,14 +1,11 @@
 import { RequestInputUserType } from "../models/user/input/updateUser-input-model";
 import { UserQueryRepository } from "../repositories/user.query-repository";
-import { basicSortQuery } from "../utils/sortQeryUtils";
 import { ResultCode } from "../validators/error-validators";
 import { hashServise } from "../utils/JWTservise";
 import { randomUUID } from "crypto";
 import { add } from "date-fns/add";
 import { UserRepository } from "../repositories/user-repository";
 import { emailAdaper } from "../utils/emailAdaper";
-import { ObjectId } from "mongodb";
-import { usersCollection } from "../BD/db";
 
 export class AuthServices {
   static async registrationUserWithConfirmation(
@@ -45,61 +42,20 @@ export class AuthServices {
         status: ResultCode.ClientError,
         errorMessage: "Some error",
       };
-    }
-
-    // ------------------------------------------------------
-    const newUserPrint = await usersCollection.findOne(new ObjectId(newUserId));
-    console.log("newUserPrint")
-    console.log(newUserPrint)
-
-    // -----------------------------------------------------------------------------
-
-    // const findedUsers = await usersCollection.findOne({ "emailConfirmation.confirmationCode": code });
-    // const findedUsers = await usersCollection.find().toArray();
-    // const emailInfoDebug = {
-    //   email: "7656077@mail.ru",
-    //   subject: "debug created user",
-    //   confirmationCode: newUser.emailConfirmation!.confirmationCode,
-    //   debug: JSON.stringify(findedUsers),
-    // };
-    // await emailAdaper.sendEmailRecoveryMessage(emailInfoDebug);
-    // -----------------------------------------------------------------------------
-
+    }  
        const emailInfo = {
       email: newUser.email,
       subject: "confirm Email",
       confirmationCode: newUser.emailConfirmation.confirmationCode,
-      // debug: JSON.stringify(findedUsers),
     };
     await emailAdaper.sendEmailRecoveryMessage(emailInfo);
-
     return {
       status: ResultCode.Success,
       data: true,
     };
   }
-
-
   static async confirmEmail(code: string): Promise<any> {
     const userForConfirmation = await UserQueryRepository.getByConfirmationCode(code);
-
-    // -----------------------------------------------------------------------------
-
-    // const findedUsers = await usersCollection.findOne({ "emailConfirmation.confirmationCode": code });
-    // const findedUsers = await usersCollection.find();
-    // const emailInfo = {
-    //   email: "7656077@mail.ru",
-    //   message: "debug",
-    //   subject: "debug confirmEmail",
-    //   confirmationCode: code,
-    //   debug: JSON.stringify(findedUsers),
-    // };
-    // await emailAdaper.sendEmailRecoveryMessage(emailInfo);
-    // -----------------------------------------------------------------------------
-
-    // console.log("--userForConfirmation----")
-    // console.log(userForConfirmation)
-
     if (!userForConfirmation) {
       return {
         status: ResultCode.ClientError,
@@ -111,7 +67,6 @@ export class AuthServices {
       return {
         status: ResultCode.ClientError,
         errorMessage:
-          // `This confirmation code ${code} already been applied`,
           JSON.stringify({ errorsMessages: [{ message: `This confirmation code ${code} already been applied`, field: "code" }] })
       };
     }
@@ -146,31 +101,8 @@ export class AuthServices {
         errorMessage: JSON.stringify({ errorsMessages: [{ message: `Not found user with ${email}`, field: "email" }] }),
       };
     }
-
-                                                    //  ---------------------------------------------------------
-                                                    const emailDebug1 = {
-                                                      email: "7656077@mail.ru",
-                                                      confirmationCode: "11111111111111111",
-                                                      subject: "11111111111111resending confirmation code",
-                                                      debug: "debug",
-                                                    };
-                                                    emailAdaper.sendEmailDebug(emailDebug1);
-                                                    //  --------------------------------------------------------
-
-    
     const emailIsConfirmed =
       userForEmailResending.emailConfirmation?.isConfirmed;
-
-                                                    //  ---------------------------------------------------------
-                                                    const emailDebug2 = {
-                                                      email: "7656077@mail.ru",
-                                                      confirmationCode: "22222222222222",
-                                                      subject: "2222222222222resending confirmation code",
-                                                      debug: "debug",
-                                                    };
-                                                    emailAdaper.sendEmailDebug(emailDebug2);
-                                                    //  --------------------------------------------------------
-
     if (emailIsConfirmed) {
       return {
         status: ResultCode.ClientError,
@@ -178,27 +110,7 @@ export class AuthServices {
       };
     }
     const newConfirmationCode = randomUUID();
-                                                  //  ---------------------------------------------------------
-                                                  const emailDebug3 = {
-                                                    email: "7656077@mail.ru",
-                                                    confirmationCode: userForEmailResending._id,
-                                                    subject: "3333333333333resending confirmation code",
-                                                    debug: userForEmailResending._id,
-                                                  };
-                                                  // @ts-ignore
-                                                  emailAdaper.sendEmailDebug(emailDebug3);
-                                                  //  --------------------------------------------------------
-                                                  // userForEmailResending._id = 65d5bf2caca141f7311c2eee
     const codeUpd  = await UserRepository.updateConfirmationCode(userForEmailResending._id, newConfirmationCode)
-                                                  //  ---------------------------------------------------------
-                                                  const emailDebug4 = {
-                                                    email: "7656077@mail.ru",
-                                                    confirmationCode: "4444444444",
-                                                    subject: "4444444444resending confirmation code",
-                                                    debug: userForEmailResending._id,
-                                                  };
-                                                  emailAdaper.sendEmailDebug(emailDebug4);
-                                                  //  --------------------------------------------------------
     if (!codeUpd) {
       return {
         status: ResultCode.ServerError,
@@ -211,16 +123,6 @@ export class AuthServices {
       subject: "resending confirmation code",
     };
      emailAdaper.sendEmailRecoveryMessage(emailInfo);
-
-    //  ---------------------------------------------------------
-    const emailDebug5 = {
-      email: "7656077@mail.ru",
-      confirmationCode: newConfirmationCode,
-      subject: "5555555555resending confirmation code",
-      debug: "debug",
-    };
-     emailAdaper.sendEmailDebug(emailDebug5);
-    //  --------------------------------------------------------
     return {
       status: ResultCode.Success,
       data: true,
