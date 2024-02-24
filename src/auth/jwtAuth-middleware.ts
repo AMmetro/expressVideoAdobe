ab01074fad26775161f52e82f213ac90b380d65a
mitskevich.andrei <mitskevich.andrei@otr.ru>
2024-02-24 22:54:44 +0300

    add coocies

--- src/auth/jwtAuth-middleware.ts
+++ src/auth/jwtAuth-middleware.ts
@@ -3,6 +3,9 @@ import { NextFunction, Request, Response } from "express";
 import { jwtServise } from "../utils/JWTservise";
 import { UserQueryRepository } from "../repositories/user.query-repository";
 import { emailAdaper } from "../utils/emailAdaper";
+import { UserServices } from "../services/userServices";
+import { ResultCode } from "../validators/error-validators";
+import { sendCustomError } from "../utils/sendResponse";
 
 export const jwtValidationMiddleware = async (
   req: Request,
@@ -10,46 +13,64 @@ export const jwtValidationMiddleware = async (
   next: NextFunction
 ) => {
   if (!req.headers.authorization) {
-
-                      // -----------------------------------------------------------
-                      // const emailInfo1 = {
-                      //   email: "7656077@mail.ru",
-                      //   confirmationCode: JSON.stringify(req.headers.authorization),
-                      //   subject: "authorization",
-                      // };
-                      //  emailAdaper.sendEmailRecoveryMessage(emailInfo1);
-                      // -----------------------------------------------------------
+    // -----------------------------------------------------------
+    // const emailInfo1 = {
+    //   email: "7656077@mail.ru",
+    //   confirmationCode: JSON.stringify(req.headers.authorization),
+    //   subject: "authorization",
+    // };
+    //  emailAdaper.sendEmailRecoveryMessage(emailInfo1);
+    // -----------------------------------------------------------
 
     res.sendStatus(401);
     return;
   }
-  const token = req.headers.authorization.split(" ")[1];
-  const userId = await jwtServise.getUserIdByAcssToken(token);
-  if (userId) {
-    const user = await UserQueryRepository.getById(userId);
-    if (!user) {
-                  // // -----------------------------------------------------------
-                  // const emailInfo1 = {
-                  //   email: "7656077@mail.ru",
-                  //   confirmationCode: "1111111111111",
-                  //   subject: "NO USER",
-                  // };
-                  //  emailAdaper.sendEmailRecoveryMessage(emailInfo1);
-                  // // -----------------------------------------------------------
-      res.sendStatus(401);
-      return;
+
+  // ----------------------------------------------------------
+  const checkAcssesToken = async (authRequest: string) => {
+    const token = authRequest.split(" ")[1];
+    const userId = await jwtServise.getUserIdByAcssToken(token);
+    if (userId) {
+      const user = await UserQueryRepository.getById(userId);
+      if (!user) {
+        return {
+          status: ResultCode.NotFound,
+          errorMessage: "Not found user with id " + userId,
+        };
+      }
+      return {
+        status: ResultCode.Success,
+        data: user,
+      };
     }
-    req.user = user;
-    next();
-    return;
+    return {
+      status: ResultCode.NotFound,
+      errorMessage: "JWT is broken",
+    };
+  };
+
+  const result = await checkAcssesToken(req.headers.authorization);
+  if (result.status === ResultCode.Success && result.data) {
+    req.user = result.data;
+    return next();
+  } else {
+    sendCustomError(res, result);
   }
-                    // -----------------------------------------------------------
-                    const emailInfo1 = {
-                      email: "7656077@mail.ru",
-                      confirmationCode: "222222222222",
-                      subject: "final"
-                    };
-                     emailAdaper.sendEmailRecoveryMessage(emailInfo1);
-                    // -----------------------------------------------------------
+
+  // ----------------------------------------------------------
+  // const token = req.headers.authorization.split(" ")[1];
+  // const userId = await jwtServise.getUserIdByAcssToken(token);
+
+  // if (userId) {
+  //   const user = await UserQueryRepository.getById(userId);
+  //   if (!user) {
+  //     res.sendStatus(401);
+  //     return;
+  //   }
+  // req.user = user;
+  // next();
+  // return;
+  // }
+
   res.sendStatus(401);
 };
--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -88,7 +88,4 @@ export class UserServices {
   }
 
 
-
-
-
 }
