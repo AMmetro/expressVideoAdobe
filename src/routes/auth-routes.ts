5b10d0051767590c1a2e90f230221b900e3e6265
mitskevich.andrei <mitskevich.andrei@otr.ru>
2024-02-24 20:38:55 +0300

    add coocies

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -94,7 +94,7 @@ authRoute.post(
             //  emailAdaper.sendEmailRecoveryMessage(emailInfo1);
             // -----------------------------------------------------------
     // const oldRefreshToken= req.cookies
-    const oldRefreshToken= req.cookies.refresh_Ttoken 
+    const oldRefreshToken= req.cookies.refresh_token 
     // console.log(oldRefreshToken)
     // console.log(oldRefreshToken)
     // console.log(oldRefreshToken)
