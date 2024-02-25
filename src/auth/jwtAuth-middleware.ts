3a5c60f5cf52ad3bda4528f6819f0a81cbca7136
AMmetro <metro.trend.now@gmail.com>
2024-02-25 18:53:29 +0300

    change logout endpoint

--- src/auth/jwtAuth-middleware.ts
+++ src/auth/jwtAuth-middleware.ts
@@ -27,65 +27,65 @@ export const jwtValidationMiddleware = async (
   }
 
   // ----------------------------------------------------------
-  // вынесено в отдельную функцию для возможности тестирвоания
-  const checkAcssesToken = async (authRequest: string) => {
-    const token = authRequest.split(" ");
+  // // вынесено в отдельную функцию для возможности тестирвоания
+  // const checkAcssesToken = async (authRequest: string) => {
+  //   const token = authRequest.split(" ");
     
-    const authMethod = token[0];
-    if (authMethod !== "Bearer") {
-      return {
-        status: ResultCode.Unauthorised,
-        errorMessage: "auth method is not Bearer",
-      };
-    }
+  //   const authMethod = token[0];
+  //   if (authMethod !== "Bearer") {
+  //     return {
+  //       status: ResultCode.Unauthorised,
+  //       errorMessage: "auth method is not Bearer",
+  //     };
+  //   }
 
-    const userId = await jwtServise.getUserIdByAcssToken(token[1]);
-    if (userId) {
-      const user = await UserQueryRepository.getById(userId);
-      if (!user) {
-        return {
-          status: ResultCode.Unauthorised,
-          errorMessage: "Not found user with id " + userId,
-        };
-      }
-      return {
-        status: ResultCode.Success,
-        data: user,
-      };
-    }
-    return {
-      status: ResultCode.Unauthorised,
-      errorMessage: "JWT is broken",
-    };
-  };
+  //   const userId = await jwtServise.getUserIdByAcssToken(token[1]);
+  //   if (userId) {
+  //     const user = await UserQueryRepository.getById(userId);
+  //     if (!user) {
+  //       return {
+  //         status: ResultCode.Unauthorised,
+  //         errorMessage: "Not found user with id " + userId,
+  //       };
+  //     }
+  //     return {
+  //       status: ResultCode.Success,
+  //       data: user,
+  //     };
+  //   }
+  //   return {
+  //     status: ResultCode.Unauthorised,
+  //     errorMessage: "JWT is broken",
+  //   };
+  // };
 
-  const result = await checkAcssesToken(req.headers.authorization);
-  if (result.status === ResultCode.Success && result.data) {
-    req.user = result.data;
-    return next();
-  } else {
-    sendCustomError(res, result);
-  }
+  // const result = await checkAcssesToken(req.headers.authorization);
+  // if (result.status === ResultCode.Success && result.data) {
+  //   req.user = result.data;
+  //   return next();
+  // } else {
+  //   sendCustomError(res, result);
+  // }
 
   // ----------------------------------------------------------
-  // if (req.headers.authorization.split(" ")[0] !== "Bearer") {
-  //   res.sendStatus(401);
-  //   return;
-  //     }
+  if (req.headers.authorization.split(" ")[0] !== "Bearer") {
+    res.sendStatus(401);
+    return;
+      }
 
-  // const token = req.headers.authorization.split(" ")[1];
-  // const userId = await jwtServise.getUserIdByAcssToken(token);
+  const token = req.headers.authorization.split(" ")[1];
+  const userId = await jwtServise.getUserIdByAcssToken(token);
 
-  // if (userId) {
-  //   const user = await UserQueryRepository.getById(userId);
-  //   if (!user) {
-  //     res.sendStatus(401);
-  //     return;
-  //   }
-  // req.user = user;
-  // next();
-  // return;
-  // }
+  if (userId) {
+    const user = await UserQueryRepository.getById(userId);
+    if (!user) {
+      res.sendStatus(401);
+      return;
+    }
+  req.user = user;
+  next();
+  return;
+  }
 
   res.sendStatus(401);
 };
