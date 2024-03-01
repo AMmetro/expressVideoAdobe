44c351666d6d856823655c4a456f0306d06ac910
AMmetro <metro.trend.now@gmail.com>
2024-03-01 18:25:39 +0300

    add api delete device validations

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -90,7 +90,7 @@ authRoute.post(
       .status(200)
       .send({ accessToken });
     } else {
-      res.sendStatus(444);
+                                                res.sendStatus(444);
       // sendCustomError(res, result);
       return
     }
@@ -138,6 +138,10 @@ authRoute.post(
   emailExistValidator,
   inputValidationMiddleware,
   async (req: RequestWithBody<RegistrationUserInputModel>, res: Response) => {
+
+                                                            res.sendStatus(408);
+                                                            return;
+
     const { password, login, email } = req.body;
     if (!password || !login || !email) {
       res.sendStatus(401);
