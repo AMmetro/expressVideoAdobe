38e18f79e81f66a2bf0599874597cab547b59a0b
AMmetro <metro.trend.now@gmail.com>
2024-03-10 14:52:32 +0300

    next

--- src/BD/db.ts
+++ src/BD/db.ts
@@ -44,7 +44,7 @@ export const runDB = async ()=>{
         console.log("DB connected...") 
         // await KittenModel.deleteMany({});
         // await BlogModel.deleteMany({});
-        await RateLimitModel.deleteMany({});
+        // await RateLimitModel.deleteMany({});
     } 
     catch(e) {
         console.log(e)
@@ -54,53 +54,3 @@ export const runDB = async ()=>{
 } 
 
 
-
-type BlogsType =
-    {
-        id: string,
-        name: string,
-        description: string,
-        websiteUrl: string,
-    }
-
-type PostsType =
-    {
-        id: string,
-        title: string,
-        shortDescription: string,
-        content: string,
-        blogId: string,
-        blogName: string,
-    }
-
-    const blogs: BlogsType[] = [
-    {
-        "id": "1",
-        "name": "blogs1111",
-        "description": "description1111",
-        "websiteUrl": "websiteUrl1111",
-    },
-    {
-        "id": "2",
-        "name": "blogs2222",
-        "description": "description22222",
-        "websiteUrl": "websiteUrl22222",
-    }
-      ]
-
-     const posts: PostsType[] = [
-    {
-        "id": "1", 
-        "title": "posts111",
-        "shortDescription": "shortDescription111",
-        "content": "content111",
-        "blogId": "1",
-        "blogName": "blogs1111",
-    }
-     ]
-
-
-export const db = {
-    blogs: blogs,
-    posts: posts
-}
