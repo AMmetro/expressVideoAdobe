a5fc14164e07455659bf3c633516f46d9cd30723
AMmetro <metro.trend.now@gmail.com>
2024-03-12 17:52:17 +0300

    try reresh

--- src/routes/comments-routes.ts
+++ src/routes/comments-routes.ts
@@ -57,6 +57,10 @@ commentsRoute.get(
       return;
     }
     const comment = await CommentsServices.composeComment(id);
+    // @ts-ignore
+    res.send({ dislikesCount: 50,
+                  likesCount: 100
+    });
     // const comment = await CommentsQueryRepository.getById(id);
   //   if (!comment) {
   //     res.sendStatus(404);
