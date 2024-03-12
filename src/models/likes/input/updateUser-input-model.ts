d3a7954e4793bd2dc37b4d55cf0495ad581c9c8b
AMmetro <metro.trend.now@gmail.com>
2024-03-12 12:51:28 +0300

    back

--- src/models/likes/input/updateUser-input-model.ts
+++
@@ -1,9 +0,0 @@
-// export type InputBlogType = UpdateBlogType & {createdAt: string, isMembership: boolean,}
-
-export type UpdateInputUserType = RequestInputUserType & {createdAt: string }
-
-export type RequestInputUserType = {
-    login: string,
-    password: string,
-    email: string,
-}
