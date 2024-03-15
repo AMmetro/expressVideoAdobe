73bdfed5756d9ac603eee472adcf53fa2b1d52d1
AMmetro <metro.trend.now@gmail.com>
2024-03-15 20:33:08 +0300

    fix names

--- src/services/postServices.ts
+++ src/services/postServices.ts
@@ -81,7 +81,7 @@ export class PostServices {
               const currentLike = await LikesModel.findOne({userId: userId, commentId: comment.id })
               currentLikeStatus = currentLike ? currentLike.myStatus : likeStatusEnum.None
             } 
-            return {...comment, likesInfo:{likesCounts, dislikesCount, myStatus: currentLikeStatus } }
+            return {...comment, likesInfo:{likesCount, dislikesCount, myStatus: currentLikeStatus } }
           }) 
           )
    const result = {...comments, items: сommentsWithLikes }
