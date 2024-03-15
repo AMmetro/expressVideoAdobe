5910943ff16ecf42df7ea2e881dce7c74839bbb5
AMmetro <metro.trend.now@gmail.com>
2024-03-15 19:34:50 +0300

    upd strucrure

--- src/services/postServices.ts
+++ src/services/postServices.ts
@@ -79,7 +79,7 @@ export class PostServices {
           const currentLike = await LikesModel.findOne({userId: userId, commentId: comment.id })
           currentLikeStatus = currentLike ? currentLike.myStatus : likeStatusEnum.None
         } 
-        return {comment, likesInfo:{likeCounts, dislikeCounts, myStatus: currentLikeStatus } }
+        return {...comment, likesInfo:{likeCounts, dislikeCounts, myStatus: currentLikeStatus } }
       }) 
 
 
